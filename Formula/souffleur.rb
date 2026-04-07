756b458cff77019d31e8c4042da8aefb405665ac
Bohdan Tsikhanovskyi <bohdan@intelligentlegalsolutions.co.uk>
2026-04-07 10:46:07 +0800

    bump to 1.1.1

--- Formula/souffleur.rb
+++ Formula/souffleur.rb
@@ -1,11 +1,11 @@
 class Souffleur < Formula
   desc "CLI tool for audio recording and transcription"
   homepage "https://github.com/bog-dance/souffleur"
-  version "1.1.0"
+  version "1.1.1"
 
   on_arm do
-    url "https://github.com/bog-dance/souffleur/releases/download/v1.1.0/souffleur-1.1.0-arm64-apple-darwin.tar.gz"
-    sha256 "8bb30df8052e7333b194c87644b082adaa393341ad8f5a1feed0b383d9b01150"
+    url "https://github.com/bog-dance/souffleur/releases/download/v1.1.1/souffleur-1.1.1-arm64-apple-darwin.tar.gz"
+    sha256 "1b3457e22e3fb6b3eb4381566e19ac6ef5de137aa627789425a413986295431e"
   end
 
   def install
